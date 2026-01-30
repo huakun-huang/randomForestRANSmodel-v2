2a2cf0e1ed84c421abe7e2eb769e308b7e81d159
Huakun Huang (黄华坤) <1519726560@qq.com>
2026-01-30 10:48:47 +0800

    Add files via upload

---
+++ noiseData/noiseData.C
@@ -0,0 +1,116 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     | Website:  https://openfoam.org
+    \\  /    A nd           | Copyright (C) 2011-2022 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+Application
+    getRANS
+
+Description
+    Output the Pk, Pw, Dk, Dw and neuro features input for RANS cases 
+    for incompressible (0) and compressible case (1)
+
+\*---------------------------------------------------------------------------*/
+
+#include "fvCFD.H"
+#include "singlePhaseTransportModel.H"
+#include "turbulentTransportModel.H"
+#include "radiationModel.H"
+#include "fvOptions.H"
+#include "rhoThermo.H"
+#include "turbulentFluidThermoModel.H"
+#include "simpleControl.H"
+#include "wallDist.H"
+#include <vector>
+#define COMPRESSIBLE 1
+#include "PINN.H"
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+int main(int argc, char *argv[])
+{
+    argList::noParallel();
+    // timeSelector::addOptions();
+    timeSelector::addOptions(true, true);
+    
+    #include "setRootCase.H"
+    #include "createTime.H"
+    #include "createMesh.H"
+    
+    
+    Info<< "Times found:" << runTime.times()[0] << endl;
+    
+    //word timess = runTime.times()[0];
+
+    instantList timeDirs = timeSelector::select0(runTime, args);
+    
+    //get the latestTime
+    int size = timeDirs.size();
+    
+    //read velocity uMean
+    
+    char latestTime[100];
+    sprintf(latestTime, "%g", timeDirs[size-1].value());
+    
+    #include "createFields.H"
+    
+    Info<<"create features for PINN"<<endl;
+    #include "createFeatures.H"
+
+    Info<<"get features for PINN"<<endl;
+    #include "getInputFeatures.H"
+    
+    mut = nut/(Uc*Lc);
+    
+    Info<<"output features for PINN"<<endl;
+    #include "outputFeatures.H"
+	data = fopen("inputFeatures.txt", "w");
+	if(data!=NULL)
+    {
+        forAll(U, celli)
+        {
+            {
+                
+                {
+                      for(int i = 0; i<inputFeaturesSize; i++)
+                      {
+                           fprintf(data, "%g\t", inputFeatures[i][celli]);
+                      }
+                     fprintf(data, "%g\t", mut[celli]);
+                     fprintf(data, "%g\t", OMEGA[celli]/std::max(STRAIN[celli], 1e-10));
+
+                    //compressibleModule
+                    fprintf(data, "%g\t", TRatio[celli]);
+
+                    fprintf(data, "\n");
+                }
+            }
+        }
+        fclose(data);
+    }
+    #include "outputResults.H"
+
+    Info<< "End\n" << endl;
+
+    return 0;
+}
+
+
+// ************************************************************************* //
